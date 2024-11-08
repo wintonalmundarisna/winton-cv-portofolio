3f719af6ce2a248e8f787bbd89ac0baaa1e3b6fa
Winton Almundarisna <wintonalmundarisna@gmail.com>
2024-11-09 06:19:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const data = [
+    {
+        image: 'bookita.png',
+        title: 'BooKita',
+        body: 'Website LARAVEL tentang CRUD dan jual beli buku secara online',
+        link: 'https://github.com/wintonalmundarisna/bookita-v2.git',
+        size: 'contain'
+    },
+    {
+        image: 'quottime.png',
+        title: 'Quottime',
+        body: 'Website LARAVEL tentang CRUD kata-kata bijak multi level',
+        link: 'https://github.com/wintonalmundarisna/collaboration.git',
+        size: 'contain'
+    },
+    {
+        image: 'sistem-pakar.png',
+        title: 'Sistem Pakar',
+        body: 'Website PHP Native tentang sistem pakar penyakit kulit metode Forward Chain',
+        link: 'https://github.com/wintonalmundarisna/sistem-pakar-metode-forward-chain.git',
+        size: 'cover'
+    },
+    {
+        image: 'blog.jpg',
+        title: 'Blog',
+        body: 'Website LARAVEL seperti google yang berisi artikel-artikel atau kumpulan blog',
+        link: 'https://github.com/wintonalmundarisna/blog-v2.git',
+        size: 'cover'
+    },
+    {
+        image: 'spp.jpg',
+        title: 'Pembayaran SPP',
+        body: 'Aplikasi JAVA Netbeans untuk keperluan transaksi pembayaran SPP',
+        link: 'https://github.com/wintonalmundarisna/aplikasi-desktop-kas-sekolah.git',
+        size: 'contain'
+    }
+];
+
+
+const certification = [
+    {
+        id: '1',
+        title: 'Bootcamp Dicoding Indosat (AWS)',
+        desc: 'Telah mengikuti kelas Cloud Practitioner Essentials  (Belajar Dasar AWS Cloud) pada 27 Juli 2022'
+    },
+    {
+        id: '2',
+        title: 'Bootcamp Dicoding Indosat (JS)',
+        desc: 'Telah mengikuti Pembelajaran Dasar Pemrograman JavaScript pada 19 Juni 2022'
+    },
+    {
+        id: '3',
+        title: 'Bootcamp Dicoding Indosat (Backend)',
+        desc: 'Telah mengikuti Pembelajaran Membuat Aplikasi Back-End untuk Pemula pada 20 Juli 2022'
+    },
+    {
+        id: '4',
+        title: 'Bootcamp CAMP404 CodeIgniter 4',
+        desc: 'Telah mengikuti Bootcamp CAMP404 CodeIgniter 4 Web Programming pada 27 Desember 2021 dengan passing grade A'
+    },
+    {
+        id: '5',
+        title: 'Bootcamp CAMP404 Basic Web Programming',
+        desc: 'Telah mengikuti Bootcamp CAMP404 Basic Web Programming pada 28 Februari 2022 dengan passing grade A'
+    },
+    {
+        id: '6',
+        title: 'Bootcamp CAMP404 LARAVEL 8',
+        desc: 'Telah mengikuti Bootcamp LARAVEL 8 Web Programming pada 29 Maret 2022 dengan passing grade A'
+    },
+];
